dea373c68dfb8fbb30f48cd2ab9d8282da0ef6e9
hank <anima923@hotmail.com>
2022-02-06 23:18:54 +0800

    add jquery css..etc

--- doc/db.md
+++ doc/db.md
@@ -1,1 +1,14 @@
 # 資料庫規劃
+* resume
+    * id - int(11)
+    * name - varchar(22)
+    * education - varchar(60)
+    * intro - text
+    * autobiography - text
+    * email
+
+* collections 
+    * id - int(11)
+    * img - varchar(22)
+    * link - varchar(60)
+    * resume_id - int(11)
--- index.php
+++ index.php
@@ -1,13 +1,42 @@
 <!DOCTYPE html>
 <html lang="en">
+
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
     <link rel="shortcut icon" href="#" type="image/x-icon">
     <title>作品集</title>
+    <link rel="stylesheet" href="./css/main.css">
+    <script src="./js/jquery-3.6.0.min.js"></script>
 </head>
+
 <body>
-    <h1>作品集</h1>
+    <nav class="menu">
+
+    </nav>
+    <div id = "intro">
+
+    </div>
+    <div id = "collections">
+        <div class="work_item">
+
+        </div>
+        <div class="work_item">
+            
+        </div>
+        <div class="work_item">
+            
+        </div>
+        <div class="work_item">
+            
+        </div>
+        
+    </div>
+    <footer class="footer">
+        <h2>Contact</h2>
+        XXX@gmail.com
+    </footer>
 </body>
+
 </html>
